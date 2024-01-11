22e414d2c99b596c3e4fbc310559eb060f2ad4af
Victor Gudilov <deepburn1983@gmail.com>
2024-01-11 11:54:55 +0300

    Написан код в 2-х вариантах. Перезалит файл блок-сехемы (небольшие правки) и дописан README.md

---
+++ ProgramOne/Program.cs
@@ -0,0 +1,60 @@
+﻿// 1-й вариант решения задачи с использованием функций (методов).
+
+int maxLength = 3;
+
+Console.Clear();
+string input = InputMessage("Введите любые данные (слова, числа, символы) через запятую: ");
+string[] newArray = input.Split(',');
+
+int sizeResult = SizeNewArray(newArray, maxLength);
+
+if (sizeResult == 0)
+{
+    Console.Clear();
+    Console.WriteLine($"[{string.Join(", ", newArray)}] - Все введённые данные длиной более 3 (трёх).");
+}
+else
+{
+    string[] resultArray = ResultArray(newArray, maxLength, sizeResult);
+    Console.Clear();
+    Console.WriteLine($"[{string.Join(", ", newArray)}] -> [{string.Join(", ", resultArray)}]");
+}
+
+string InputMessage(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine()!;
+}
+
+int SizeNewArray(string[] array, int length)
+{
+    int sizeNewArray = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Trim().Length <= length)
+        {
+            sizeNewArray++;
+        }
+    }
+
+    return sizeNewArray;
+}
+
+string[] ResultArray(string[] array, int length, int size)
+{
+    string[] outputArray = new string[size];
+    int outputIndex = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        string trimmed = array[i].Trim();
+        if (trimmed.Length <= length)
+        {
+            outputArray[outputIndex] = trimmed;
+            outputIndex++;
+        }
+    }
+
+    return outputArray;
+}
---
+++ ProgramTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿// 2-й вариант решения задачи без использования функций (методов).
+
+Console.WriteLine("Введите значения через запятую: ");
+string input = Console.ReadLine()!;
+string[] inputArray = input.Split(",");
+
+string[] outputArray = new string[inputArray.Length];
+int outputIndex = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    string trimmed = inputArray[i].Trim();
+    if (trimmed.Length <= 3)
+    {
+        outputArray[outputIndex] = trimmed;
+        outputIndex++;
+    }
+}
+if (outputIndex == 0)
+{
+    Console.Clear();
+    Console.WriteLine($"[{string.Join(", ", inputArray)}] -> Все введенные данные больше трех символов.");
+}
+else
+{
+    string[] resultArray = new string[outputIndex];
+    Array.Copy(outputArray, resultArray, outputIndex);
+
+    Console.Clear();
+    Console.WriteLine($"[{string.Join(", ", inputArray)}] -> [{string.Join(", ", resultArray)}]");
+}
